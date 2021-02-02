f29778a21f6695e408b397e13ac207c8fdd713e9
Sheena Puthanpurayil <sheena24789@gmail.com>
2021-02-02 20:23:55 +0200

    remove debug logging

--- platform_in/app/__init__.py
+++ platform_in/app/__init__.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import requests
 
 logging.basicConfig(level=logging.INFO)
-elastic_apm = ElasticAPM()
+#elastic_apm = ElasticAPM()
 
 success_response_object = {"status": "success"}
 success_code = 200
@@ -26,7 +26,7 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     # set up extensions
-    elastic_apm.init_app(app)
+    #elastic_apm.init_app(app)
 
     def get_ds_id(thing, sensor):
         # """
@@ -35,12 +35,13 @@ def create_app(script_info=None):
         # """
 
         payload = {"thing": thing, "sensor": sensor}
-        logging.debug(f"getting datastream id {payload}")
-        resp = requests.get(app.config["DATASTREAMS_ENDPOINT"], params=payload)
-        # resp = requests.get("http://host.docker.internal:1338/datastream", params=payload)
+        logging.info(f"getting datastream id {payload}")
+        #resp = requests.get(app.config["DATASTREAMS_ENDPOINT"], params=payload)
+        resp = requests.get("http://host.docker.internal:1338/datastream", params=payload)
         logging.debug(f"response: {resp.json()} ")
 
         id = -1
+        print(resp.json())
         ds = resp.json()["Datastreams"]
         if len(ds) == 1:
             id = ds[0]["datastream_id"]
@@ -60,7 +61,7 @@ def create_app(script_info=None):
     def put_sentilonoise_data():
         try:
             data = request.get_json()
-            logging.info(data)
+            logging.debug(data)
             data_streams = data["sensors"]
 
             for data_stream in data_streams:
@@ -98,18 +99,18 @@ def create_app(script_info=None):
                 payload = {"topic": topic, "observation": observation}
 
                 headers = {"Content-type": "application/json"}
-                resp = requests.post(
-                    app.config["OBSERVATIONS_ENDPOINT"],
-                    data=json.dumps(payload),
-                    headers=headers,
-                )
-                # resp = requests.post("http://host.docker.internal:1337/observation", data=json.dumps(payload), headers=headers)
+                # resp = requests.post(
+                #     app.config["OBSERVATIONS_ENDPOINT"],
+                #     data=json.dumps(payload),
+                #     headers=headers,
+                # )
+                resp = requests.post("http://host.docker.internal:1337/observation", data=json.dumps(payload), headers=headers)
 
             return success_response_object, success_code
 
         except Exception as e:
             logging.error("Error at %s", "data to kafka", exc_info=e)
-            elastic_apm.capture_exception()
+            #elastic_apm.capture_exception()
             return failure_response_object, failure_code
 
     return app
